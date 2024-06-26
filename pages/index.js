ad844b6f6d014f9a580ac00f32c5925190d9b4b7
Khatanzorig <khatanzorig.b@tavanbogd.com>
2024-06-27 01:31:01 +0800

    portofolia

---
+++ pages/index.js
@@ -0,0 +1,165 @@
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import Image from 'next/image'
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m an linux developer based in Mongolia!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Amane Marin
+          </Heading>
+          <p>Kernel Enjoyer Geek ( Developer / Designer / Anime Fan )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/images/amane.jpg"
+              alt="Profile image"
+              width="100"
+              height="100"
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Amane is a full-stack developer based in Ulaanbaatar with a
+          passion for building digital services/stuff he wants. He has a knack
+          for all things launching products, from planning and designing all the
+          way to solving real-life problems with code. When not online, he loves
+          hanging out with his camera. Currently, he is living off of his own
+          product called{' '}
+        </Paragraph>
+        <Box align="center" my={4}>
+          <Button
+            as={NextLink}
+            href="/works"
+            scroll={false}
+            rightIcon={<ChevronRightIcon />}
+            colorScheme="teal"
+          >
+            My portfolio
+          </Button>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2000</BioYear>
+          Born in Ulaanbaatar, Mongolia.
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Completed the Bachelor&apos;s Program in the Graduate Mongolian 
+          University of Science and Technology.
+        </BioSection>
+        <BioSection>
+          <BioYear>2019</BioYear>
+          Worked at Able Soft as intern and developer.
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Worked at Sod Mongol LLC
+        </BioSection>
+        <BioSection>
+          <BioYear>2023 to present</BioYear>
+          Working as Next Social Platform LLC
+        </BioSection>
+      </Section> 
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          Social Network
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/maririn312" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @maririn312
+              </Button>
+            </Link>
+          </ListItem>
+          
+          <ListItem>
+            <Link href="https://twitter.com/m1or3n" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @m1or3n
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/m1or3n" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @m1or3n
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Home
+export { getServerSideProps } from '../components/chakra'
