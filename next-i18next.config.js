8c8552123d0252dbdf301c299a44414e5884f92f
kyuna312 <khatanzorigb@gmail.com>
2024-08-21 20:18:01 +0800

    changes

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,4 +4,11 @@ module.exports = {
     locales: ['en', 'ja'], // Add your supported locales here
     defaultLocale: 'en',
   },
+    localePath: './public/locales', // Path where your locale files are stored
+    localeStructure: '{{lng}}/{{ns}}',
+    localeExtension: 'json',
+    load: 'currentOnly',
+    react: {
+      useSuspense: false,
+    },
 };
