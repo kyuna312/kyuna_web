d84640acda5a32d261c7eead50135b6d7c84af39
kyuna <31735410+kyuna312@users.noreply.github.com>
2024-09-05 11:29:16 +0800

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -7,7 +7,6 @@ import nomadx from '../public/images/works/nomadx.png';
 import mongolnet from '../public/images/works/mongolnet.png';
 import nyanmarkdown from '../public/images/works/nyanmarkdown.png';
 import madoka_react from '../public/images/works/madoka_react.png';
-import nyanvim from '../public/images/works/nyanvim.png';
 
 const Projects = () => {
  	return (
@@ -20,27 +19,22 @@ const Projects = () => {
 				<SimpleGrid columns={[ 1, 1, 2 ]} gap={6}>
 					<Section>
 						<ProjectGridItem id="NomadX" title="NomadX" thumbnail={nomadx} url='https://nomadx.world' >
-						  モンゴルのNFTマーケットプレイスWebアプリケーションおよびモバイルアプリケーション
+		                                  NomadxはNFTマーケットプレイスで、WebアプリではReact、Vite、Tailwindを活用してフロントエンドを開発しました。モバイルアプリにはFlutterを採用し、快適なユーザー体験を提供しました。本プロジェクトでは、フロントエンドおよびモバイル開発者として携わりました。
 						</ProjectGridItem>					
 					</Section>
 					<Section>
 						<ProjectGridItem id="mongolnet" title="mongolnet" thumbnail={mongolnet} url='https://mongol.net'>
-						  React、Vite、Tailwind CSS、NestJS、Flutter、GraphQLを使用してMongol.netという国立ソーシャルネットワークプラットフォームを開発しました。このプロジェクトに関する具体的な問い合わせや詳細については、お問い合わせください。
-						</ProjectGridItem>
+		                                  Mongol.netはモンゴルの国立ソーシャルネットワークプラットフォームです。このプロジェクトでは、フロントエンド、バックエンド、モバイルアプリの開発に携わりました。バックエンドにはNestJSとGraphQL、WebフロントエンドにはReact、Vite、Tailwind CSSを使用し、モバイルアプリにはFlutterを採用しました。詳細はお気軽にお問い合わせください。				  
+		                                </ProjectGridItem>
 					</Section>
 					<Section>
 						<ProjectGridItem id="NyanMarkDown " title="NyanMarkDown " thumbnail={nyanmarkdown} url='https://github.com/kyuna312/NyanVim'>
-						  NyanMarkDownカスタムMarkdownエディタ
+						  NyanMarkDownはElectron.jsを使って開発したカスタムMarkdownエディタです。大学時代に自分でプロジェクトとして取り組み、デスクトップアプリケーションとして提供しています。
 						</ProjectGridItem>
 					</Section>
 					<Section>
 						<ProjectGridItem id="madoka_react " title="madoka_react " thumbnail={madoka_react} url='https://madoka-kappa.vercel.app'>
-						  私のカスタムReact JS ViteベースのSASSアニメーションウェブ
-						</ProjectGridItem>
-					</Section>
-					<Section delay={0.1}>
-						<ProjectGridItem id="nyanvim" title="nyanvim" thumbnail={nyanvim} url='https://github.com/kyuna312/NyanVim'>
-						  私のカスタムNeoVimプロジェクト
+						  madoka_reactは、自分で作成したプロジェクトです。このプロジェクトでは、React JS、Vite、SASSを使用してアニメーション付きのウェブサイトを開発しました。
 						</ProjectGridItem>
 					</Section>
 				</SimpleGrid>
